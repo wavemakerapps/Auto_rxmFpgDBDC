ac607dcf02b02403d659d091539aa772ee98cd24
root <root@d3a2e0704417>
2017-06-13 15:58:41 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_rxmFpgDBDC",
+  "homePage" : "Main",
+  "name" : "Auto_rxmFpgDBDC",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
